160e5a0f506828e98fe931cc8ceb2b4af1f98e4e
TitusDoggo <titusstudiosmediagroup@gmail.com>
2022-03-31 02:31:26 +1300

    Update

---
+++ README.md
@@ -0,0 +1,27 @@
+# Base Station Power Management
+
+Using [nouser2013's](https://github.com/nouser2013) [lighthouse-v2-manager](https://github.com/nouser2013/lighthouse-v2-manager) python script and snippets from [BOLL7708's](https://github.com/BOLL7708) [OpenVRStartup](https://github.com/BOLL7708/OpenVRStartup) automatic command file executor.
+
+Licenses for bundled code are found in their respective folders inside `./include/`.
+
+---
+
+**Required dependencies:**
+    * .NET 6.0 Required. Not bundled into executable
+    * Windows 10, at least 17xx build
+    * BLE / Bluetooth 4.0 dongle installed and connected (not a BGAPI one!)
+    * Python 3
+        * Pythonnet `pip3 install pythonnet`
+        * bleak `pip3 install bleak`
+
+---
+
+> **CAUTION:** Installation instructions are not correct. There is no way of inputting your base station mac addresses without building the application yourself **yet..** This feature will be implemented soon with GUI and more!
+
+## Installation:
+
+1. Download latest binary from the [Releases](https://github.com/TitusStudiosMediaGroup/basestation_power_management/releases) page
+2. Execute binary atleast once before using with SteamVR.
+3. You're done! Every time you launch SteamVR the executable will launch in the background, and close on exit of SteamVR.
+
+> **NOTE:** Launching the executable on its own will launch SteamVR
---
+++ basestation_power_management.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Valve.VR;
+
+namespace basestation_power_management
+{
+    class Core
+    {
+        // Put your lighthouse MAC addresses in the array below seperated by commas.
+        private static readonly string[] LH_Addresses = new string[] {"D1:57:E0:68:C5:9F","D4:F7:08:5A:48:76"};
+        private static bool DEBUG = false;
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int CW_HIDE = 0;
+        const int CW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            var ConsoleWindow = GetConsoleWindow();
+            Console.Title = "Base Station Power Management Console";
+            if(args.Contains("debug")) DEBUG = true;
+            if(DEBUG == false) ShowWindow(ConsoleWindow,CW_HIDE);
+
+            InjectApplication();
+            PrintColor("Putting base stations to WAKE state.",ConsoleColor.Blue);
+            LighthouseState(true);
+            
+            var CheckEvents = true;
+
+            while(CheckEvents)
+            {
+                var vrEvent = new VREvent_t();
+                uint eventSize = (uint)Marshal.SizeOf(vrEvent);
+                OpenVR.System.PollNextEvent(ref vrEvent,eventSize);
+
+                if((EVREventType)vrEvent.eventType == EVREventType.VREvent_Quit)
+                {
+                    OpenVR.System.AcknowledgeQuit_Exiting();
+                    PrintColor("[OPENVR] Acknowledge Quit, Exiting.",ConsoleColor.Green);
+                    OpenVR.Shutdown();
+                    PrintColor("Putting base stations to SLEEP state.",ConsoleColor.Blue);
+                    LighthouseState(false);
+                    CheckEvents = false;
+                }
+            }
+
+            PrintColor("Shutting down.. Press ENTER to close.",ConsoleColor.Gray);
+            if(DEBUG == true) Console.ReadLine();
+        }
+
+        private static void PrintColor(string text,ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        private static void InjectApplication()
+        {
+            var error = EVRInitError.None;
+            OpenVR.Init(ref error,EVRApplicationType.VRApplication_Overlay);
+
+            if(!OpenVR.Applications.IsApplicationInstalled("titus.basestation_power_management"))
+            {
+                PrintColor("[OPENVR] OpenVR application manifest: \"titus.basestation_power_management\" doesnt exist!",ConsoleColor.Red);
+                OpenVR.Applications.AddApplicationManifest(Path.GetFullPath("./app.vrmanifest"),false);
+                OpenVR.Applications.SetApplicationAutoLaunch("titus.basestation_power_management",true);
+                PrintColor("[OPENVR] Injected OpenVR application manifest!",ConsoleColor.Green);
+            }
+
+            PrintColor("[OPENVR] Application manifest already injected.",ConsoleColor.Green);
+        }
+
+        private static void LighthouseState(bool State)
+        {
+            for (int i = 0; i < LH_Addresses.GetLength(0); i++)
+			{
+                PrintColor("[LH] ("+LH_Addresses[i]+") state changed to " + (State == true ? "UP" : "DOWN"),ConsoleColor.Blue);
+			}
+
+            using (Process Manager = new Process())
+            {
+                Manager.StartInfo.FileName = "python3 ./include/lighthouse/lighthouse-v2-manager.py";
+                Manager.StartInfo.Arguments = (State == true ? "on " : "off ") + String.Join(" ", LH_Addresses);
+                Manager.StartInfo.UseShellExecute = false;
+                Manager.StartInfo.RedirectStandardOutput = true;
+                Manager.StartInfo.CreateNoWindow = true;
+                Manager.Start();
+
+                PrintColor(Manager.StandardOutput.ReadToEnd(),ConsoleColor.Blue);
+
+                Manager.WaitForExit();
+            };
+        }
+    }
+}
